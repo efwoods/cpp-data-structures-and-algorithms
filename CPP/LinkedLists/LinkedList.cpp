9662074f3dc60e88c4b466bcc1e16020138f6b22
Evan Woods <22337004+efwoods@users.noreply.github.com>
2025-01-24 12:36:31 -0500

    wrote append method in c++ for a Linked List

--- CPP/LinkedLists/LinkedList.cpp
+++ CPP/LinkedLists/LinkedList.cpp
@@ -1,9 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
 class Node{
-    private:
+    public:
         int value;
         Node* next;
-    
-    public:
+
         Node(int value){
             this->value = value;
             next = nullptr;
@@ -23,8 +26,63 @@ class LinkedList{
             tail = newNode;
             length = 1;
         }
+
+        // Destructor
+        ~LinkedList() {
+            Node* temp = head;
+            while(head){
+                head = head->next;
+                delete temp;
+                temp = head;
+            }
+        }
+
+        // Append
+        void append(int value){
+            Node* newNode = new Node(value);
+            if(tail){
+                tail->next = newNode;
+                tail = newNode;
+            } else {
+                head = newNode;
+                tail = newNode;
+            }
+            length++;
+        }
+
+        void printList() {
+            Node* temp = head;
+            while(temp){
+                cout << temp->value << endl;
+                temp = temp->next;
+            }
+        }
+
+        void getHead() {
+            cout << "Head: " << head->value << endl;
+        }
+
+        void getTail(){
+            cout << "Tail: " << tail->value << endl;
+        }
+
+        void getLength(){
+            cout << "Length: " << length << endl;
+        }
 };
 
 int main(){
-    LinkedList* myLinkedList = new LinkedList(4);
+    // LinkedList* myLinkedList = new LinkedList(4);
+    // myLinkedList->getHead();
+    // myLinkedList->getTail();
+    // myLinkedList->getLength();
+
+    // myLinkedList->printList();
+
+    // Testing Append
+    LinkedList* myAppendedList = new LinkedList(1);
+    myAppendedList->printList();
+
+    myAppendedList->append(2);
+    myAppendedList->printList();
 }
