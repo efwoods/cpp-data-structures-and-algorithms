661189abaf592eebfba34e3c43523e717631f2a9
Evan Woods <22337004+efwoods@users.noreply.github.com>
2025-01-30 10:49:50 -0500

    added pop and test for pop successfully

--- cpp/Classes/tests/test_Stack.cpp
+++ cpp/Classes/tests/test_Stack.cpp
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
 #include "../Stack/Stack.h"
 #include <iostream>
+#include <limits.h>
+
+using namespace std;
 
 TEST(StackTest, Constructor){
     Stack* stack = new Stack(0);
@@ -16,4 +19,23 @@ TEST(StackTest, PrintStatements){
     Stack* stack = new Stack(0);
     stack->printStack();
     stack->getHeight();
+}
+
+TEST(StackTest, Push){
+    Stack* stack = new Stack(0);
+    EXPECT_TRUE(stack != nullptr);
+
+    stack->push(1);
+    EXPECT_TRUE(stack->getTop()->value == 1);
+    EXPECT_TRUE(stack->getHeight() == 2);
+}
+
+TEST(StackTest, Pop){
+    Stack* stack = new Stack(0);
+    EXPECT_TRUE(stack != nullptr);
+
+    int poppedValue = stack->pop();
+    EXPECT_TRUE(poppedValue == 0);
+    poppedValue = stack->pop();
+    EXPECT_TRUE(poppedValue == INT_MIN);
 }
