3a9cf06c676d8ff1658199c842e62d2ffe9182b0
Evan Woods <22337004+efwoods@users.noreply.github.com>
2025-01-31 11:53:12 -0500

    created and successfully tested allKeys function

--- cpp/Classes/HashTable/HashTable.cpp
+++ cpp/Classes/HashTable/HashTable.cpp
@@ -41,10 +41,10 @@ int HashTable::hash(string key){
 void HashTable::set(string key, int value){
     int index = hash(key);
     Node* newNode = new Node(key, value);
-    if (dataMap[index] == nullptr){
+    if (dataMap[index] == nullptr){ // set 
         dataMap[index] = newNode;
     } else {
-        Node* temp = dataMap[index];
+        Node* temp = dataMap[index]; // iterating through the linked list at the index location during a collision;
         while(temp->next != nullptr){
             temp = temp->next;
         }
@@ -56,10 +56,22 @@ int HashTable::get(string key){
     int index = hash(key);
     Node* temp = dataMap[index];
     while (temp != nullptr) {
-        if (temp->key == key) {
+        if (temp->key == key) { // iterate through the linked list if the key of the dataMap index location is not the hashed key;
             return temp->value;
         }
         temp = temp->next;
     }
-    return 0;
+    return 0; // if the key was not found in the hash table;
+}
+
+vector<string> HashTable::keys() {
+    vector<string> allKeys;
+        for(int i = 0; i < SIZE; i++){
+            Node* temp = dataMap[i];
+            while(temp != nullptr){
+                allKeys.push_back(temp->key);
+                temp = temp->next;
+            }
+        }
+        return allKeys;
 }
